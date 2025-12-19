49a15256666d65caed0b91fd51047dd1b9c028b6
NeoOrangePanda <h.codingal@gmail.com>
2025-12-19 18:02:34 +0600

    commit

--- ACP/Count of Vowels/main.py
+++ ACP/Count of Vowels/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import re
 
 app = Flask(__name__)
 
@@ -13,11 +14,13 @@ def count():
     vowels = ['a', 'e', 'i', 'o', 'u']
     v_count = 0
 
+    highlighted = re.sub(r'([aeiouAEIOU])', r'<span class="text-green-500">\1</span>', string)
+
     for letter in string_modified:
         if letter in vowels:
             v_count += 1
 
-    return render_template('index.html', v_count=v_count, s=string)
+    return render_template('index.html', v_count=v_count, s=highlighted)
 
 if __name__ == "__main__":
     app.run(debug=True, port=24011)

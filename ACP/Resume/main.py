a3cb349fc75b0e896ffd856e7e9488d1dd4e360e
NeoOrangePanda <h.codingal@gmail.com>
2026-01-02 00:53:00 +0600

    commit

--- ACP/Resume/main.py
+++ ACP/Resume/main.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Blueprint, session, jsonify
+import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "app_secret_keys"
+
+def get_db_connection():
+    conn = sqlite3.connect('ACP\Resume\info.db')
+    return conn
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    selected_set = 'set1'
 
-@app.route('/admin')
-def admin():
-    return render_template('admin_login.html')
+    conn = get_db_connection()
+    info_set = conn.execute('SELECT name, email, task, phone_num FROM resume_info WHERE setID = ?', (selected_set,)).fetchone()
+
+    splited_name = str(info_set[0]).split()
+
+    return render_template('index.html', info_set=info_set, s=splited_name)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ ACP/Resume/sql.sql
@@ -0,0 +1,3 @@
+INSERT INTO resume_info (setID, name, email, task, phone_num) VALUES
+    ('set1', 'Raaeid Akram', 'h.codingal@gmail.com', 'Frontend Website Developer', '+8801713441449'),
+    ('set2', 'Lorem Ipsum', 'loremipsum@email.com', 'Lorem Ipsum', '+88011111199999')
